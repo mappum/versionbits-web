5119f70ffe0b952f32581c05e07504103326056b
Matt Bell <mappum@gmail.com>
2016-10-12 15:29:07 -0700

    Moved bitcoin code out of Web Worker

--- src/main.js
+++ src/main.js
@@ -3,87 +3,160 @@
 const vdom = require('virtual-dom')
 const mainLoop = require('main-loop')
 const hyperx = require('hyperx')
-
-const EventEmitter = require('events')
 const assign = require('object-assign')
-const work = require('webworkify')
-const workerStream = require('workerstream')
-require('setimmediate')
+const level = require('level-browserify')
+const pump = require('pump')
+const { toHash } = require('bitcoin-util')
+const webcoin = require('webcoin')
+const versionbits = require('versionbits')
 
-var state = {
-  chainTip: { height: 0, hash: '' },
-  vbSync: { height: 0, hash: '' },
-  peers: [],
-  deployments: []
+const networks = {
+  bitcoin: {
+    params: require('webcoin-bitcoin'),
+    checkpoints: [{
+      height: 399168,
+      header: {
+        version: 4,
+        prevHash: toHash('0000000000000000074f9edbfc07648dc74392ba8248f0983ffea63431b3bc20'),
+        merkleRoot: toHash('0ed1b9a40f94aec95e2843369bdcabaa42f860c82391c54874a7c193d7268eaa'),
+        timestamp: 1455885256,
+        bits: 403093919,
+        nonce: 3889666804
+      }
+    }]
+  }
 }
+networks.bitcoin.params.net.webSeeds = [ 'ws://localhost:8192' ]
 
-var hx = hyperx(vdom.h)
-var loop = mainLoop(state, render, vdom)
-document.querySelector('#app').appendChild(loop.target)
+function main () {
+  var state = {
+    chain: {
+      block: null,
+      synced: false
+    },
+    vbits: {
+      block: null,
+      synced: false
+    },
+    peers: [],
+    deployments: []
+  }
 
-var worker = createNodeWorker()
-worker.on('peer', (peer) => {
-  state.peers.push(peer)
-  loop.update(state)
-})
-worker.on('block', (tip) => {
-  loop.update(assign(state, { chainTip: tip }))
-})
-worker.on('vbits', (data) => {
-  if (data.sync) state.vbSync = data.sync
-  state.deployments = data.deployments
-  loop.update(state)
-})
+  var hx = hyperx(vdom.h)
+  var loop = mainLoop(state, render(hx), vdom)
+  document.querySelector('#app').appendChild(loop.target)
 
-function createNodeWorker () {
-  var nw = new EventEmitter()
-  var worker = nw.worker = work(require('./nodeWorker.js'))
-  var stream = nw.stream = workerStream(worker)
-  stream.on('data', ({ event, data }) => nw.emit(event, data))
-  return nw
+  var updateState = (d) => loop.update(assign(state, d))
+
+  var node = createNode('bitcoin')
+  var vbits = createVbits('bitcoin', node)
+  var updatePeers = () => updateState({ peers: node.peers.peers })
+  node.peers.on('peer', (peer) => {
+    updatePeers()
+    peer.once('disconnect', updatePeers)
+  })
+  node.chain.on('block', (block) => {
+    state.chain.block = block
+    updateState()
+  })
+  node.on('synced', () => {
+    var chain = state.chain
+    chain.synced = true
+    updateState({ chain })
+  })
+  vbits.on('block', (block) => {
+    console.log('vbits block', block)
+    state.vbits.block = block
+    updateState()
+  })
+  vbits.once('ready', () => {
+    var { deployments } = vbits
+    updateState({ deployments })
+  })
+  vbits.on('update', updateState)
+  node.start()
 }
+main()
 
-function render (state) {
-  return hx`<div>
-    <h1>blockchain sync:</h1>
-    <span>
-      ${state.chainTip.height}
-      <span> - </span>
-      ${state.chainTip.hash}
-    </span>
-    <h1>versionbits sync:</h1>
-    <span>
-      ${state.vbSync.height}
-      <span> - </span>
-      ${state.vbSync.hash}
-    </span>
-    <h1>peers (${state.peers.length}):</h1>
-    <ul>
-    ${state.peers.map((peer) => hx`
-      <li>
-        <span>${peer.version.userAgent}</span>
-      </li>
-    `)}
-    </ul>
-    <h1>deployments:</h1>
-    <ul>
-    ${state.deployments.map((dep) => {
-      if (dep.unknown && dep.count < 10) return
-      return hx`<li>
-        <span>${dep.name}</span>
-        <span> - </span>
-        <span>${dep.status}</span>
-        <span> - </span>
-        <span>${dep.count}</span>
-      </li>`
-    })}
-    </ul>
-    <button onclick=${reset}>Reset State</button>
-  </div>`
+function render (hx) {
+  return (state) =>
+    hx`<div>
+      <h1>blockchain sync:</h1>
+      ${state.chain.block ? hx`
+        <span>
+          ${state.chain.block.height}
+          <span> - </span>
+          ${state.chain.block.header.getId()}
+        </span>
+      ` : null}
+      ${state.chain.synced ? hx`
+        <p>Up to date.</p>
+      ` : null}
+      <h1>versionbits sync:</h1>
+      ${state.vbits.block ? hx`
+        <span>
+          ${state.vbits.block.height}
+          <span> - </span>
+          ${state.vbits.block.header.getId()}
+        </span>
+      ` : null}
+      <h1>peers (${state.peers.length}):</h1>
+      <ul>
+      ${state.peers.map((peer) => hx`
+        <li>
+          <span>${peer.version.userAgent}</span>
+        </li>
+      `)}
+      </ul>
+      <h1>deployments:</h1>
+      <ul>
+      ${state.deployments.map((dep) => {
+        if (dep.unknown && dep.count < 10) return
+        return hx`<li>
+          <span>${dep.name}</span>
+          <span> - </span>
+          <span>${dep.status}</span>
+          <span> - </span>
+          <span>${dep.count}</span>
+        </li>`
+      })}
+      </ul>
+      <button onclick=${reset}>Reset State</button>
+    </div>`
 }
 
 function reset () {
   window.indexedDB.deleteDatabase('IDBWrapper-bitcoin.node')
   window.indexedDB.deleteDatabase('IDBWrapper-bitcoin.versionbits')
   window.location.reload()
+}
+
+function createNode (id) {
+  const { params, checkpoints } = networks[id]
+  if (checkpoints) params.blockchain.checkpoints = checkpoints
+  const nodeDb = level(`${id}.node`)
+  return webcoin(params, nodeDb)
+}
+
+function createVbits (id, { chain }) {
+  let { params } = networks[id]
+  let vbitsDb = level(`${id}.versionbits`)
+  let vbits = versionbits(params.versionbits, vbitsDb)
+  chain.onceReady(() => {
+    console.log('blockchain is ready')
+    chain.getBlockAtHeight(399168, (err, block) => {
+      if (err) return console.log(err)
+      console.log(block)
+      vbits.getHash((err, hash) => {
+        if (err) return console.log(err)
+        pump(
+          chain.createReadStream({ from: hash || block.header.getHash() }),
+          vbits,
+          (err) => {
+            if (err) console.log(err.stack)
+          })
+      })
+    })
+  })
+  return vbits
 }
